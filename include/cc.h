dc6014276502c3e6b40407dca7e61d77f259025c
Donghyeon Kim <kdh0429@snu.ac.kr>
2023-01-06 13:34:57 +0900

    Add double support force diff regulation

--- include/cc.h
+++ include/cc.h
@@ -60,7 +60,7 @@ public:
     float phase_ = 0.0;
 
     bool is_on_robot_ = false;
-    bool is_write_file_ = false;
+    bool is_write_file_ = true;
     Eigen::Matrix<double, MODEL_DOF, 1> q_dot_lpf_;
 
     Eigen::Matrix<double, MODEL_DOF, 1> q_init_;
