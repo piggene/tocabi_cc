001d73facc21214e6231e254cd12a387580d09f0
Donghyeon Kim <kdh0429@snu.ac.kr>
2023-05-05 13:25:53 +0900

    Reduce quat bias to 3.14/300, and use 10 time history of 1 skip

--- include/cc.h
+++ include/cc.h
@@ -34,8 +34,8 @@ public:
     static const int num_actuator_action = 12;
     static const int num_cur_state = 44;
     static const int num_cur_internal_state = 31;
-    static const int num_state_skip = 2;
-    static const int num_state_hist = 5;
+    static const int num_state_skip = 1;
+    static const int num_state_hist = 10;
     static const int num_state = num_cur_internal_state*num_state_hist+num_action*(num_state_hist-1);
     static const int num_hidden = 256;
 
